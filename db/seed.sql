1b477fa9397c6b009d1015d9eda6849873b4b2f6
Daniel V <main@dvasquez4155.com>
2020-03-25 22:50:31 -0700

    Moved sql files

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ('John', "Doe", 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ('Mike', "Chan", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id) values ('Ashley', "Rodriguez", 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ('Kevin', "Tupik", 4,3);
+INSERT INTO employee (first_name, last_name, role_id) values ('Malia', "Brown", 5);
+INSERT INTO employee (first_name, last_name, role_id) values ('Sarah', "Lourd", 6);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ('Tom', "Allen", 7, 6);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ('Christian', "Echenrode", 3, 2);
